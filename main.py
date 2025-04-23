7e675008ddd04bfa47d3d2013641bde7f5c8ac4b
David Scalais <david.scalais@student.uantwerpen.be>
2025-04-23 19:44:22 +0200

    feature sending coordinates to terminal

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
-# main.py -- put your code here!
+import sys
+sys.path.append('/flash/shields')
+
+import time
+
+import gc
+import pycom
+from lib.L76GNSS import L76GNSS
+from lib.pycoproc_1 import Pycoproc
+from network import WLAN
+wlan = WLAN()
+wlan.antenna(WLAN.EXT_ANT)
+
+pycom.heartbeat(False)
+pycom.rgbled(0x0A0A08) # white
+ 
+time.sleep(2)
+gc.enable()
+ 
+py = Pycoproc(Pycoproc.PYTRACK)
+ 
+time.sleep(1)
+l76 = L76GNSS(py, timeout=30, buffer=512)
+
+time.sleep(1)
+ 
+while (True):
+    coord = l76.coordinates(debug=True)
+    print()
+    print(coord)
+    print("{} - {}".format(coord, gc.mem_free()))
+    print('Sleep for 5 seconds.')
+    time.sleep(5)
