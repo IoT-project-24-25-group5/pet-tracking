36a3bf254df50f31846ea7a6c86cb684aa9de4ef
David Scalais <david.scalais@student.uantwerpen.be>
2025-04-24 19:25:41 +0200

    feat sending location to server

--- main.py
+++ main.py
@@ -8,8 +8,20 @@ import pycom
 from lib.L76GNSS import L76GNSS
 from lib.pycoproc_1 import Pycoproc
 from network import WLAN
-wlan = WLAN()
-wlan.antenna(WLAN.EXT_ANT)
+import machine
+import socket
+import ssl
+
+
+HOST = 'iot.philippevoet.dev'
+
+
+# swith signal towards exteranl antenna
+wlan = WLAN(mode=WLAN.STA)
+wlan.connect(ssid='stop using our Telenet', auth=(WLAN.WPA2, '1020304050'))
+while not wlan.isconnected():
+    machine.idle()
+print("WiFi connected succesfully")
 
 pycom.heartbeat(False)
 pycom.rgbled(0x0A0A08) # white
@@ -20,14 +32,46 @@ gc.enable()
 py = Pycoproc(Pycoproc.PYTRACK)
  
 time.sleep(1)
-l76 = L76GNSS(py, timeout=30, buffer=512)
+l76 = L76GNSS(py, timeout=60, buffer=512)
 
 time.sleep(1)
  
 while (True):
     coord = l76.coordinates(debug=True)
-    print()
-    print(coord)
+    
+    lat, lon = coord[0], coord[1] 
     print("{} - {}".format(coord, gc.mem_free()))
-    print('Sleep for 5 seconds.')
-    time.sleep(5)
+    
+    """lat = "51.203141"
+    lon = "4.420835"""
+
+    if lat is not None and lon is not None:
+        try:
+            path = '/location'
+            addr = socket.getaddrinfo(HOST, 443)[0][-1]
+            s = socket.socket()
+            ss = ssl.wrap_socket(s)
+            ss.connect(addr)
+
+            # Send HTTPS POST manually (raw HTTP)
+            payload = '{{"latitude": {}, "longitude": {}}}'.format(lat, lon)
+            request = (
+                "POST {} HTTP/1.1\r\n"
+                "Host: {}\r\n"
+                "Content-Type: application/json\r\n"
+                "Content-Length: {}\r\n"
+                "\r\n"
+                "{}"
+            ).format(path, HOST, len(payload), payload)
+
+            ss.send(request)
+            print("Data sent to server")
+
+            print(ss.recv(256))
+            ss.close()
+            
+        except Exception as e:
+            print("error sending coordinates" , e)
+    
+    print('Sleep for 20 seconds.')
+    time.sleep(20)
